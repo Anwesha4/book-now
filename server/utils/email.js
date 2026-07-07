2ee59327a3e6485474e4f3320a4398b46bc98905
Anwesha <anwesha.ganguly04@gmail.com>
2026-07-07 21:38:21 +0530

    Update SMTP configuration

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,11 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS,
+    },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
 const sendBookingEmail = async (userEmail, userName, eventTitle) => {
